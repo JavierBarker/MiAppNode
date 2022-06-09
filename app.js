4c49538c15b014cf7d4ff7d99fd3b3e307c986a7
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-09 17:47:49 -0600

    add: database connection and new dependencies

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const sequelize = require('./databases/database');
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('server in port: 3000');
-});
+sequelize
+  .sync()
+  .then((connect) => {
+    //console.log(connect);
+    app.listen(process.env.PORT || 3000, () => {
+      console.log('server in port: 3000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });

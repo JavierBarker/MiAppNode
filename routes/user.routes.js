f6cc4c54c8b932bf45049a62c808f599d34f7a64
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-10 17:07:57 -0600

    add: login route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/user.controller');
 
 router.get('/', userController.getUsers);
 router.post('/add', userController.postAddUser);
+router.post('/login', userController.postLogin);
 
 module.exports = router;

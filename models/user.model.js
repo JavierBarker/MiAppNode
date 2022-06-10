f341b43987a1af485d4e8ac4ecef910ab01b4477
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-10 10:04:18 -0600

    add: models folder and user model

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../databases/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});

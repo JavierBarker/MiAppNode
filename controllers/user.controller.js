9e385c88f32c2030d7d5d6ef97b3f422934f7dc6
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-13 17:51:22 -0600

    update: login function of userController

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const config = require('../config/config.json');
 const User = require('../models/user.model');
 
 exports.getUsers = async (req, res, next) => {
@@ -30,13 +31,25 @@ exports.postAddUser = async (req, res, next) => {
 };
 
 exports.postLogin = async (req, res, next) => {
-  const params = req.body;
-  const user = await User.findOne({ where: { email: params.email } });
-  const matchPassword = await bcrypt.compare(params.password, user.password);
+  try {
+    const params = req.body;
+    const user = await User.findOne({ where: { email: params.email } });
+    const matchPassword = await bcrypt.compare(params.password, user.password);
 
-  if (matchPassword) {
-    res.status(200).send('Correct Password');
-  } else {
-    res.status(201).send('Incorrect Password');
+    if (matchPassword) {
+      const payload = {
+        user: user,
+      };
+      const token = jwt.sign(payload, config.key, { expiresIn: 31536000 });
+
+      return res.status(200).send({
+        message: 'Correct Login',
+        token: token,
+      });
+    } else {
+      return res.status(400).send('Incorrect Password');
+    }
+  } catch (error) {
+    next(error);
   }
 };

068e69ef58b59d2f9a15dbad685ec60576b1b4c9
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-14 17:59:33 -0600

    add: updateProduct and addProduct functions

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -7,14 +7,31 @@ exports.getToken = async (req, res, next) => {
 exports.postAddProduct = async (req, res, next) => {
   try {
     const params = req.body;
-    const product = await req.decode.user.createProduct({
+    const product = await Product.create({
       name: params.name,
       price: params.price,
       description: params.description,
+      user_id: req.decode.user.id,
     });
+
     return res.status(200).send(product);
   } catch (error) {
     next(error);
-    console.log(error);
+  }
+};
+
+exports.putUpdateProduct = async (req, res, next) => {
+  try {
+    const params = req.body;
+    const product = await Product.findOne({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (product === null) {
+      res.status(400).send('no se encontro el producto');
+    }
+  } catch (error) {
+    next(error);
   }
 };

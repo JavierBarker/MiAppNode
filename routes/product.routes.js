1cce34e81796fe327a4af5f8bfddf3b1b0681d18
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-14 15:11:54 -0600

    add: addProduct path

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -4,5 +4,6 @@ const productController = require('../controllers/product.controller');
 const { verifyToken } = require('../middlewares/auth');
 
 router.get('/token', verifyToken, productController.getToken);
+router.post('/add', verifyToken, productController.postAddProduct);
 
 module.exports = router;

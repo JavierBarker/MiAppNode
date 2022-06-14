f74d59b26e587e8ca86baed9bffb9dee3dfca4af
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-14 15:10:43 -0600

    add: funtion addProduct

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,3 +1,20 @@
+const Product = require('../models/product.model');
+
 exports.getToken = async (req, res, next) => {
   res.status(200).send(req.decode);
+};
+
+exports.postAddProduct = async (req, res, next) => {
+  try {
+    const params = req.body;
+    const product = await req.decode.user.createProduct({
+      name: params.name,
+      price: params.price,
+      description: params.description,
+    });
+    return res.status(200).send(product);
+  } catch (error) {
+    next(error);
+    console.log(error);
+  }
 };

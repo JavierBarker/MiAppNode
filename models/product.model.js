806932ec90bb9e7576f4d50e9fefcba7b18944e9
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-14 11:31:55 -0600

    add: model product

---
+++ models/product.model.js
@@ -0,0 +1,25 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const sequelize = require('../databases/database');
+
+const Product = sequelize.define('product', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;

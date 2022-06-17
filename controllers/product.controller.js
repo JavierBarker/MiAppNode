d2e164dd47eb029747bc743abd3ac0aff3fbbf4b
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-17 17:08:36 -0600

    update: updateProducts and add delete and get functions of products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -4,6 +4,15 @@ exports.getToken = async (req, res, next) => {
   res.status(200).send(req.decode);
 };
 
+exports.getProducts = async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.status(200).send(products);
+  } catch (error) {
+    next(error);
+  }
+};
+
 exports.postAddProduct = async (req, res, next) => {
   try {
     const params = req.body;
@@ -30,6 +39,29 @@ exports.putUpdateProduct = async (req, res, next) => {
     });
     if (product === null) {
       res.status(400).send('no se encontro el producto');
+    }
+
+    await product.update({
+      name: params.name || product.name,
+      price: params.price || product.price,
+      description: params.description || product.description,
+    });
+    const save = await product.save();
+
+    if (save) {
+      res.status(200).send(save);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findByPk(req.params.id);
+    if (product) {
+      const productDeleted = product.destroy();
+      res.status(200).send(productDeleted);
     }
   } catch (error) {
     next(error);

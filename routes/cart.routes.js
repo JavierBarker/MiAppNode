421d1af00719a766aa628a670e29bc4f64e49a3d
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-23 11:58:47 -0600

    add: crud paths for products to cart

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -4,5 +4,14 @@ const cartController = require('../controllers/cart.controller');
 const { verifyToken } = require('../middlewares/auth');
 
 router.post('/:id', verifyToken, cartController.postAddProductToCart);
-
+router.put(
+  '/changeQuantity/:id',
+  verifyToken,
+  cartController.putChangeQuantity
+);
+router.delete(
+  '/deleteToCart/:id',
+  verifyToken,
+  cartController.deleteDeleteToCart
+);
 module.exports = router;

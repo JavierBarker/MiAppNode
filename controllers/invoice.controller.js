0fe1e49e81f72db9862516e5c19aaf808abc07a1
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-23 17:54:25 -0600

    update: postCreateInvoice

--- controllers/invoice.controller.js
+++ controllers/invoice.controller.js
@@ -1,1 +1,13 @@
-exports.postCrateInvoice = async (req, res, next) => {};
+const Invoice = require('../models/invoice.model');
+const InvoiceProduct = require('../models/invoice_product.model');
+const Cart = require('../models/cart.model');
+const CartProduct = require('../models/cart_product.model');
+
+exports.postCrateInvoice = async (req, res, next) => {
+  const cart = await Cart.findOne({ where: { user_id: req.decode.user.id } });
+  const cartProduct = await CartProduct.findAll({
+    where: { cart_id: cart.id },
+  });
+
+  console.log(cartProduct);
+};

0fd09ffce7646dc5c037da663c1af903ebaeaa56
JavierBarker <hernandez.javier2003@gmail.com>
2022-06-23 11:54:37 -0600

    update: products to cart crud

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -4,31 +4,64 @@ const Product = require('../models/product.model');
 
 exports.postAddProductToCart = async (req, res, next) => {
   const cart = await Cart.findOne({ where: { user_id: req.decode.user.id } });
-  let newQuantity = 1;
 
   const product = await Product.findByPk(req.params.id);
   if (product === null) res.status(500).send('No se encontro el producto');
 
   const cartProduct = await CartProduct.findOne({
-    where: { cart_id: cart.id },
+    where: { cart_id: cart.id, product_id: product.id },
   });
 
   if (cartProduct != null) {
-    const myProduct = await cartProduct.find(
-      (products) => (products.product_id = product.id)
-    );
-
-    res.status(200);
+    const newQuantity = cartProduct.quantity + 1;
+    const addProduct = await cartProduct.update({ quantity: newQuantity });
+    //const addProduct = cartProduct.save();
+    res.status(200).send(addProduct);
   } else {
     const addProduct = await CartProduct.create({
-      quantity: newQuantity,
+      quantity: 1,
       cart_id: cart.id,
       product_id: product.id,
     });
 
     res.status(200).send({ msg: 'Producto Agregado', cart: addProduct });
   }
-  console.log(cart, req.decode.user.id);
+};
 
-  console.log(cartProduct);
+exports.putChangeQuantity = async (req, res, next) => {
+  const cart = await Cart.findOne({ where: { user_id: req.decode.user.id } });
+
+  const product = await Product.findByPk(req.params.id);
+  if (product === null) res.status(500).send('No se encontro el producto');
+
+  const cartProduct = await CartProduct.findOne({
+    where: { cart_id: cart.id, product_id: product.id },
+  });
+
+  if (req.query.quantity == 0) {
+    const deleteProduct = await cartProduct.destroy();
+    res.status(200).send(deleteProduct);
+  }
+  if (cartProduct != null) {
+    const changeQuantity = await cartProduct.update({
+      quantity: req.query.quantity,
+    });
+    //const changeQuantity = cartProduct.save();
+    res.status(200).send(changeQuantity);
+  }
+};
+
+exports.deleteDeleteToCart = async (req, res, next) => {
+  const cart = await Cart.findOne({ where: { user_id: req.decode.user.id } });
+
+  const product = await Product.findByPk(req.params.id);
+  if (product === null) res.status(500).send('No se encontro el producto');
+
+  const cartProduct = await CartProduct.findOne({
+    where: { cart_id: cart.id, product_id: product.id },
+  });
+
+  const deleteProduct = await cartProduct.destroy();
+
+  res.status(200).send(deleteProduct);
 };
